8dc27dd86d1a4f833d4b60dfada2334a35fdb452
Lital Porat <poratlital@gmail.com>
2022-05-13 10:45:34 +0300

    server and db connection

---
+++ backend/config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log("MongoDB connection SUCCESS");
+  } catch (error) {
+    console.error("MongoDB connection FAILED");
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
