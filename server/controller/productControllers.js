40478a457c3114180b6dd32f7b151dce18ff2308
Ravid Eliyahu <ravid1233@gmail.com>
2022-05-17 13:26:19 +0300

    adding add product to db

--- server/controller/productControllers.js
+++ server/controller/productControllers.js
@@ -1,12 +1,15 @@
 const res = require("express/lib/response");
 const Product = require("../models/Product");
+const logger = require('../logger');
+require('../validators.js')();
 
 const getAllProducts = async (req, res) => {
   try {
     const products = await Product.find({});
+    logger.info("GET /api/products request has been accepted");
     res.json(products);
   } catch (error) {
-    console.error(error);
+    logger.error(error);
     res.status(500).json({ message: "Server Error!" });
   }
 };
@@ -14,15 +17,28 @@ const getAllProducts = async (req, res) => {
 const getProductById = async (req, res) => {
   try {
     const products = await Product.findById(req.params.id);
+    logger.info(`GET /api/products/${req.params.id} request has been accepted`);
     res.json(products);
   } catch (error) {
-    console.error(error);
+    logger.error(error);
     res.status(500).json({ message: "Server Error!" });
   }
 };
 
-const addProduct = (req, res) => {
-  res.json(req.body);
+const addProduct = async (req, res) => {
+  try {
+    const { error } = validateProduct(req.body);
+    if (error) {
+      logger.error(error);
+      res.status(400).send({ status: 400, error});
+      return;
+    }
+    logger.info(`POST /api/products/ request has been accepted`);
+    await Product.create(req.body);
+  } catch (e) {
+    logger.error(e);
+    res.status(400).send(e);
+  }
 }
 
 module.exports = {
