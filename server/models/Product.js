463e53cbb27efaca3b01b37500bf9de61a82543b
RavidEliyahu <ravid1233@gmail.com>
2022-05-22 10:08:02 +0300

    editing schema

--- server/models/Product.js
+++ server/models/Product.js
@@ -1,26 +1,85 @@
 const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
-  name: {
+  display_name: {
     type: String,
     required: true,
+    minlength: 0,
+    maxlength: 15,
+    validate: {
+      validator: v => v.length > 0 && v.length < 15,
+      message: props => `${props.value} length is not valid!`
+    }
   },
   description: {
-    type: String,
+    type: [String],
+    required: true,
+  },
+  more_info: {
+    type: Object,
+    materials: [String],
+    origin: String,
+    model_size: String,
+    model_height: Number,
+    instructions: [String],
     required: true,
   },
   price: {
     type: Number,
     required: true,
   },
-  countInStock: {
-    type: Number,
-    required: true,
+  discount: {
+    type: Number
   },
-  imageUrl: {
-    type: String,
+  stock: {
     required: true,
+    xs: {
+      type: Number,
+      default: 0
+    },
+    s:  {
+      type: Number,
+      default: 0
+    },
+    m:  {
+      type: Number,
+      default: 0
+    },
+    l:  {
+      type: Number,
+      default: 0
+    },
+    os:  {
+      type: Number,
+      default: 0
+    },
+    xl:  {
+      type: Number,
+      default: 0
+    }
   },
+  type: {
+    required: true,
+    type: String
+  },
+  images: {
+    required: true,
+    display: [String],
+    details: [String]
+  },
+  available_sizes: [String],
+  search_keywords: [String],
+  amount_sold: Number,
+  similar_products: [mongoose.SchemaTypes.ObjectId],
+  created_at: {
+    type: Date,
+    immutable: true,
+    default: () => new Date()
+  },
+  updated_at: {
+    type: Date,
+    default: () => new Date()
+  }
 });
 
 const Product = mongoose.model("product", productSchema);
--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ require("./config/db")
         const PORT = process.env.PORT || 5000;
         app.listen(PORT, () => logger.info(`Server running on port ${PORT}`));
     })
-    .catch((e) => { 
-        logger.error(e) 
+    .catch((e) => {
+        logger.error(e);
     });
 
