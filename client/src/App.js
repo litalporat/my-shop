99755e084839ac4e4236e02d5744cdaead9e31ad
Nimi <Nimrobo@Gmail.com>
2022-05-23 18:50:03 +0300

    Login

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Login from './Login';
+import Register from './Register';
 
 // Screens
 import HomePage from './pages/Home'
@@ -12,8 +14,12 @@ import Navbar from './components/Navbar';
 function App() {
     return (
         <Router>
+            {/* <main className = "App">
+                <Register />
+            </main> */}
             <Navbar/>
                 <Routes>
+                    <Route exact path="/login" element={<Login/>} />
                     <Route exact path="/" element={<HomePage/>}/>
                     {/* <Route exact path="/product/:id" element={<ProductPage/>}/> */}
                     <Route exact path="/cart" element={<CartPage/>} />
