491dc567b84d50021cc8bb2345c67c642339e7e6
Nimi <Nimrobo@Gmail.com>
2022-06-02 11:17:50 +0300

    - Added home shortcut on logo - Removed underscore from logo (on App.css) - Profile button - Linked 'Start Tour' to shop

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,6 +1,8 @@
+import Catalog from "../../components/Catalog";
 import "./Home.css";
 import Photo from "../../images/photo.jpg";
 import ArrowBtn from "../../components/ArrowBtn";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
   return (
@@ -12,12 +14,15 @@ const HomePage = () => {
         <div className="home-head">
           <h1> Welcome To Our App</h1>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Iste unde quibusdam, laborum aperiam veniam et nisi
-            architecto doloremque commodi tempore eveniet vero velit
-            accusamus, ipsam ex nesciunt provident reiciendis! Nihil.
+            {" "}
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste unde
+            quibusdam, laborum aperiam veniam et nisi architecto doloremque
+            commodi tempore eveniet vero velit accusamus, ipsam ex nesciunt
+            provident reiciendis! Nihil.
           </p>
-          <ArrowBtn content={"Start Tour"} />
+          <Link to="/shop">
+            <ArrowBtn content={"Start Tour"} />
+          </Link>
         </div>
       </div>
     </div>
