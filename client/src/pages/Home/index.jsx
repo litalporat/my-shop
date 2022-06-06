8e016d8e8e548501982c14fd72283ef67d3b1347
Bar <barshoshani274@gmail.com>
2022-06-06 18:03:26 +0300

    check

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,20 +1,8 @@
 import "./Home.css";
-import Footer from '../../components/Sections/Footer'
 import Photo from "../../images/photo.jpg";
 import ArrowBtn from "../../components/ArrowBtn";
-import HeartContext from "../../Contexts/HeartContext";
-import { useContext, useState } from "react";
 
 const HomePage = () => {
-  const { hearts } = useContext(HeartContext);
-  const [foryou, setForyou] = useState([
-    "628a3950cd566118fe095cbc",
-    "628a3950cd566118fe095cbd",
-    "628a3950cd566118fe095cc5",
-  ]);
-
-  console.log(foryou);
-
   return (
     <div className="home-body">
       <div className="a">
@@ -24,11 +12,10 @@ const HomePage = () => {
         <div className="home-head">
           <h1> Welcome To Our App</h1>
           <p>
-            {" "}
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste unde
-            quibusdam, laborum aperiam veniam et nisi architecto doloremque
-            commodi tempore eveniet vero velit accusamus, ipsam ex nesciunt
-            provident reiciendis! Nihil.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Iste unde quibusdam, laborum aperiam veniam et nisi
+            architecto doloremque commodi tempore eveniet vero velit
+            accusamus, ipsam ex nesciunt provident reiciendis! Nihil.
           </p>
           <ArrowBtn content={"Start Tour"} />
         </div>
