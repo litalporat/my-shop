6b22dc87ea04dd93ca4173ff0e9f83d61c92c53b
Bar <barshoshani274@gmail.com>
2022-06-08 15:49:03 +0300

    check

--- client/src/pages/Shop/index.jsx
+++ client/src/pages/Shop/index.jsx
@@ -36,18 +36,12 @@ const ProductPage = (props) => {
       });
   }, []);
 
-<<<<<<< HEAD
-=======
-  console.log(data);
-
->>>>>>> 5aaee44b87298379e0b000b2ddfe97eb2f6415d7
   // Filtering the data
   useEffect(() => {
     if (data) {
       let tempData = [...data];
       let newData = [];
       let filteredData = [];
-<<<<<<< HEAD
       console.log(activeFilters);
       Object.keys(activeFilters).map((key, index) => {
         activeFilters[key].forEach((value) => {
@@ -60,21 +54,10 @@ const ProductPage = (props) => {
         newData.forEach((e) =>
           e.forEach((d) => filteredData.push(d))
         );
-=======
-      console.log(filters);
-      Object.keys(filters).map((key, index) => {
-        filters[key].forEach((value) => {
-          newData.push(tempData.filter((prod) => prod[key].includes(value)));
-        });
-      });
-      if (newData.length > 0) {
-        newData.forEach((e) => e.forEach((d) => filteredData.push(d)));
->>>>>>> 5aaee44b87298379e0b000b2ddfe97eb2f6415d7
         setViewData(filteredData);
       } else {
         setViewData(tempData);
       }
-<<<<<<< HEAD
     }
   }, [activeFilters]);
 
@@ -108,39 +91,6 @@ const ProductPage = (props) => {
       let tempData = [...data];
       setViewData(tempData);
     }
-=======
-    }
-  }, [filters]);
-  const togglePopup = () => {
-    setIsOpen(!isOpen);
-  };
-  const changeContent = (product) => {
-    setcontent(product);
-  };
-  const filterByParam = (param, value) => {
-    let newFilters = { ...filters };
-    if (!newFilters[param]) newFilters[param] = [];
-    newFilters[param].push(value);
-    setFilters(newFilters);
-  };
-  const deletefilterByParam = (param, value) => {
-    let newFilters = { ...filters };
-    newFilters[param] = newFilters[param].filter((e) => e != value);
-    setFilters(newFilters);
-  };
-  const filterByPrice = (value) => {
-    let tempData = [...data];
-    tempData = tempData.filter(
-      (product) => product.price >= value[0] && product.price <= value[1]
-    );
-    setViewData(tempData);
-  };
-  const sortByInt = (param, order) => {
-    if (order === "Default") {
-      let tempData = [...data];
-      setViewData(tempData);
-    }
->>>>>>> 5aaee44b87298379e0b000b2ddfe97eb2f6415d7
     if (order === "High To Low") {
       let tempData = [...viewData];
       tempData = tempData.sort((a, b) => a[param] - b[param]);
@@ -179,7 +129,6 @@ const ProductPage = (props) => {
           delFilterFunc={deletefilterByParam}
           filterByPrice={filterByPrice}
         />
-<<<<<<< HEAD
         <BasicButton
           title={`Number Of Products: ${data && viewData.length}`}
         />
@@ -187,10 +136,6 @@ const ProductPage = (props) => {
           sortByInt={sortByInt}
           sortByString={sortByString}
         />
-=======
-        <BasicButton title={`Number Of Products: ${data && viewData.length}`} />
-        <SorterComp sortByInt={sortByInt} sortByString={sortByString} />
->>>>>>> 5aaee44b87298379e0b000b2ddfe97eb2f6415d7
       </div>
       <div className="shop-list">
         {data &&
@@ -204,13 +149,9 @@ const ProductPage = (props) => {
               onCart={() => addProduct(product)}
             />
           ))}
-<<<<<<< HEAD
         {isOpen && (
           <Popup content={content} handleClose={togglePopup} />
         )}
-=======
-        {isOpen && <Popup content={content} handleClose={togglePopup} />}
->>>>>>> 5aaee44b87298379e0b000b2ddfe97eb2f6415d7
       </div>
     </div>
   );
