371750872242818b3607129c883e669d6b1bd889
RavidEliyahu <ravid1233@gmail.com>
2022-06-14 14:08:11 +0300

    removing old server

--- server/config/redisConnect.js
+++
@@ -1,8 +0,0 @@
-const redis = require('redis');
-
-const redisClient = redis.createClient();
-client.on('connect', () => {
-    console.log('redis is connected!');
-});
-
-module.exports = redisClient;
